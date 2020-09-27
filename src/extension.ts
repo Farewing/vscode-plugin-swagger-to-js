27833d188d631d55443bd26575511a984bb099dc
深屿 <lhr245262@alibaba-inc.com>
2020-09-27 16:42:40 +0800

    fix: 联调

--- src/extension.ts
+++ src/extension.ts
@@ -2,27 +2,26 @@ import * as vscode from "vscode";
 import { generate } from "./index";
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "swagger-to-ts" is now active!');
+  console.log('Congratulations, your extension "extension" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
 
   // buffett-flow/info
-  const get = vscode.commands.registerCommand("swagger-to-ts.get", () =>
+  const get = vscode.commands.registerCommand("extension.get", () =>
     generate("get")
   );
 
-  const post = vscode.commands.registerCommand(
-    "swagger-to-ts.post",
-    async () => await generate("post")
+  const post = vscode.commands.registerCommand("extension.post", () =>
+    generate("post")
   );
 
-  const put = vscode.commands.registerCommand("swagger-to-ts.put", () =>
+  const put = vscode.commands.registerCommand("extension.put", () =>
     generate("put")
   );
 
-  const deleteM = vscode.commands.registerCommand("swagger-to-ts.delete", () =>
+  const deleteM = vscode.commands.registerCommand("extension.delete", () =>
     generate("delete")
   );
 
