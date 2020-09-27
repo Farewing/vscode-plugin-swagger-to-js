a049dd93730bb7f34366d2847b48d31b9df06200
深屿 <lhr245262@alibaba-inc.com>
2020-09-27 13:07:06 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -8,26 +8,25 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
 
-  ///buffett-flow/info
-  const disposable = vscode.commands.registerCommand("swagger-to-ts.get", () =>
+  // buffett-flow/info
+  const get = vscode.commands.registerCommand("swagger-to-ts.get", () =>
     generate("get")
   );
 
-  const disposable2 = vscode.commands.registerCommand(
+  const post = vscode.commands.registerCommand(
     "swagger-to-ts.post",
-    () => generate("post")
+    async () => await generate("post")
   );
 
-  const disposable3 = vscode.commands.registerCommand("swagger-to-ts.put", () =>
+  const put = vscode.commands.registerCommand("swagger-to-ts.put", () =>
     generate("put")
   );
 
-  const disposable4 = vscode.commands.registerCommand(
-    "swagger-to-ts.delete",
-    () => generate("delete")
+  const deleteM = vscode.commands.registerCommand("swagger-to-ts.delete", () =>
+    generate("delete")
   );
 
-  context.subscriptions.push(disposable, disposable2, disposable3, disposable4);
+  context.subscriptions.push(get, post, put, deleteM);
 }
 
 // this method is called when your extension is deactivated
